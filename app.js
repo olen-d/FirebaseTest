5b5fdcbe3e5ff1341de27b482f9170166b11afb1
Salvador Dieffenbach <zogwarg@hotmail.com>
2019-01-13 22:50:09 -0500

    Initial

---
+++ app.js
@@ -0,0 +1,12 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBzH6JdvXccH6gtM3r0fjpLueV_zp_LOlE",
+    authDomain: "fir-test-6f4f0.firebaseapp.com",
+    databaseURL: "https://fir-test-6f4f0.firebaseio.com",
+    projectId: "fir-test-6f4f0",
+    storageBucket: "fir-test-6f4f0.appspot.com",
+    messagingSenderId: "872501139917"
+};
+firebase.initializeApp(config);
+db = firebase.database();
