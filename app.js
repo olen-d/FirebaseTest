c706179e2863c42edbd37545c63e30a2cde8e6bc
Salvador Dieffenbach <zogwarg@hotmail.com>
2019-01-13 23:38:11 -0500

    Fixed loop of death

--- app.js
+++ app.js
@@ -14,19 +14,38 @@ db = firebase.database();
 let token = "";
 let user = "";
 let provider = new firebase.auth.GithubAuthProvider();
-firebase.auth().signInWithRedirect(provider);
-firebase.auth().getRedirectResult().then(function(result) {
-    if (result.credential) {
-        token = result.credential.accessToken;
-        user = result.user;
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      console.log(displayName);
+      // ...
+    } else {
+      // User is signed out.
+        firebase.auth().signInWithRedirect(provider);
+        firebase.auth().getRedirectResult().then(function(result) {
+            if (result.credential) {
+                token = result.credential.accessToken;
+                user = result.user;
+            }
+        }).catch(function(error) {
+            let c = error.code;
+            let m = error.message;
+            let e = error.email;
+            let cr = error.credential;
+            console.log(c);
+            console.log(m);
+            console.log(e);
+            console.log(cr);
+        });
     }
-}).catch(function(error) {
-    let c = error.code;
-    let m = error.message;
-    let e = error.email;
-    let cr = error.credential;
-    console.log(c);
-    console.log(m);
-    console.log(e);
-    console.log(cr);
 });
+
+
