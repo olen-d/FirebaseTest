c1fd9687bcbc6770881a2c2a0d66ae8b99bec886
Salvador Dieffenbach <zogwarg@hotmail.com>
2019-01-16 02:01:19 -0500

    l

--- app.js
+++ app.js
@@ -10,42 +10,64 @@ var config = {
     messagingSenderId: "872501139917"
 };
 firebase.initializeApp(config);
-db = firebase.database();
+let db = firebase.database();
 
-let token = "";
-//let user = "";
-let provider = new firebase.auth.GithubAuthProvider();
-
-let usersRef = db.ref("/users")
 const hGlobal = new Object();
 
+// Initialize Firebase
+
+
+let usersRef = db.ref("/users")
+
+// Using a redirect
+let provider = new firebase.auth.TwitterAuthProvider();
+
+firebase.auth().getRedirectResult().then(function(result) {
+    if (result.credential) {
+      
+        // For accessing the Twitter API.
+        hGlobal["token"] = result.credential.accessToken;
+        hGlobal["secret"] = result.credential.secret;
+    }
+    hGlobal["user"] = result.user;
+  });
+  
+// Start a sign in process for an unauthenticated user.
+
+
 firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-      let displayName = user.displayName;
-      let email = user.email;
-      let emailVerified = user.emailVerified;
-      let photoURL = user.photoURL;
-      let isAnonymous = user.isAnonymous;
-      let userId = user.uid;
-      hGlobal["userId"] = userId;
-      let providerData = user.providerData;
-      console.log(displayName);
-      console.log(user.uid);
-      const userData = {
-          name: displayName,
-          email: email,
-          photo: photoURL
-      }
-      addUser(userId,userData);
-           // ...
+    if (hGlobal.user) {
+        // User is signed in
+        hGlobal["displayName"] = user.displayName;
+        hGlobal["photoURL"] = user.photoURL;
+        hGlobal["userId"] = user.uid;
+
+        let providerData = user.providerData;
+
+        // See if the user exists
+        if (userExists(hGlobal.userId)) {
+            const lastLogin = { lastLogin: firebase.database.ServerValue.TIMESTAMP };
+            db.ref(`/users/${hGlobal.userId}`).set(lastLogin, (error) => {
+                (error ? console.log("Errors handled " + error) : console.log("Last login successfully updated. "));
+            });
+        } else {
+            // User doesn't exist, add them to the Firebase Users
+            const userData = {
+                name: hGlobal.displayName,
+                photo: hGlobal.photoURL,
+                joined: firebase.database.ServerValue.TIMESTAMP,
+                lastLogin: firebase.database.ServerValue.TIMESTAMP
+            }
+            addUser(hGlobal.userId,userData);   
+        }
     } else {
-      // User is signed out.
+      // User is signed out
         firebase.auth().signInWithRedirect(provider);
         firebase.auth().getRedirectResult().then(function(result) {
             if (result.credential) {
-                token = result.credential.accessToken;
-                var user = result.user;
+                hGlobal["token"] = result.credential.accessToken;
+                hGlobal["secret"] = result.credential.secret;
+                hGlobal["user"]  = result.user;
             }
         }).catch(function(error) {
             let c = error.code;
@@ -69,9 +91,9 @@ const userExists = (userId) => {
 } 
 
 const addUser = (userId,userData) => {
-    if(!userExists(userId)) {
+    if(!userExists(userId)) {   
         db.ref(`/users/${userId}`).set(userData, (error) => {
             (error ? console.log("Errors handled " + error) : console.log("User successfully added to the database. "));
         });
-    }   
+    }    
 }
